200882917f5c5c3328df21390724c781a327e0b3
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-02-05 16:29:19 +0000

    Only include our controllers in swagger
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/main/java/sweng/penelope/SpringFoxConfig.java
+++ src/main/java/sweng/penelope/SpringFoxConfig.java
@@ -13,6 +13,7 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
-                .select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+                .select().apis(RequestHandlerSelectors.basePackage("sweng.penelope.controllers"))
+                .paths(PathSelectors.any()).build();
     }
 }
