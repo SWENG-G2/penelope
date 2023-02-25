07a65bc24b373a4109078c2b121b52008a92dd36
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-02-25 15:45:17 +0000

    Cleanup dead code from key auth
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/main/java/sweng/penelope/controllers/KeyPairController.java
+++ src/main/java/sweng/penelope/controllers/KeyPairController.java
@@ -4,6 +4,8 @@ import java.security.KeyPair;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +20,15 @@ public class KeyPairController {
     @Autowired
     private KeyPair serverKeyPair;
 
+    @Value("${penelope.api-key}")
+    private String keyHeader;
+
     @GetMapping(path = "/key")
+    @Cacheable(value = "key")
     public ResponseEntity<String> getMethodName() {
         String key = Base64.getEncoder().encodeToString(serverKeyPair.getPublic().getEncoded());
 
-        return ResponseEntity.ok().header("key", key).build();
+        return ResponseEntity.ok().header(keyHeader, key).build();
     }
     
 }
