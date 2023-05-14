a15f1e97b50fc51d2e96ff1df60e9e341c1367c0
dz940 <dz940@york.ac.uk>
2023-05-14 14:35:18 +0100

    tweeking UI

--- src/main/java/sweng/penelope/xml/CampusXML.java
+++ src/main/java/sweng/penelope/xml/CampusXML.java
@@ -42,27 +42,27 @@ public class CampusXML extends CommonXML {
      */
     public void addBird(String name, String description, Long id, String imageURL) {
         Element duckSlide = presentation.addElement("slide").addAttribute(WIDTH, SLIDE_WIDTH)
-                .addAttribute(HEIGHT, "200")
+                .addAttribute(HEIGHT, "110")
                 .addAttribute("title", Long.toString(id));
 
         // Title
         duckSlide.addElement("text").addAttribute(FONT_NAME, FONT).addAttribute(FONT_SIZE, FONT_SIZE_TITLE_SM)
                 .addAttribute(COLOUR, BLACK).addAttribute(X_COORDINATE, "560") // (480 + 40 (image)) + 40
-                .addAttribute(Y_COORDINATE, "30").addAttribute(WIDTH, MATCH_PARENT).addAttribute(HEIGHT, WRAP_CONTENT)
+                .addAttribute(Y_COORDINATE, "10").addAttribute(WIDTH, MATCH_PARENT).addAttribute(HEIGHT, WRAP_CONTENT)
                 .addText(name);
 
         // Description
         duckSlide.addElement("text").addAttribute(FONT_NAME, FONT).addAttribute(FONT_SIZE, FONT_SIZE_BODY)
                 .addAttribute(COLOUR, BLACK).addAttribute(X_COORDINATE, "560").addAttribute(WIDTH, "1300")
                 .addAttribute(HEIGHT, WRAP_CONTENT)
-                .addAttribute(Y_COORDINATE, "68") // 28 (FONT_SIZE_TITLE_SM) + 2*20 (FONT_SIZE_BODY)
+                .addAttribute(Y_COORDINATE, "48") // 28 (FONT_SIZE_TITLE_SM) + 2*20 (FONT_SIZE_BODY)
                 .addText(formatDescription(description));
 
         // Image
         duckSlide.addElement("image").addAttribute("url", imageURL).addAttribute(WIDTH, "480") // 480 = 100 * (1920/200)
                                                                                                // * (100/200)
                 .addAttribute(HEIGHT, "100").addAttribute(X_COORDINATE, "40")
-                .addAttribute(Y_COORDINATE, "40");
+                .addAttribute(Y_COORDINATE, "5");
 
         incrementNumSlides();
     }
