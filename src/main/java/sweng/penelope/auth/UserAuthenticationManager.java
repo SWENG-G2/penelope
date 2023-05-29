e2ed0cd83c6c9f9236ff611851858b3280e63cd4
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-05-29 16:53:01 +0100

    Clean up UserAuthenticationManager
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/main/java/sweng/penelope/auth/UserAuthenticationManager.java
+++ src/main/java/sweng/penelope/auth/UserAuthenticationManager.java
@@ -6,10 +6,8 @@ import java.time.ZonedDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -30,46 +28,41 @@ public class UserAuthenticationManager implements AuthenticationManager {
     private PasswordEncoder passwordEncoder;
 
     @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        try {
-            String[] credentials = authentication.getPrincipal().toString().split("=");
-            String username = credentials[0];
-            String password = credentials[1];
-            String timestamp = credentials[2];
+    public Authentication authenticate(Authentication authentication) {
+        String[] credentials = authentication.getPrincipal().toString().split("=");
+        String username = credentials[0];
+        String password = credentials[1];
+        String timestamp = credentials[2];
 
-            String claim = authentication.getCredentials().toString();
+        String claim = authentication.getCredentials().toString();
 
-            // Compare timestamp
-            ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/London"));
-            ZonedDateTime sentAt = ZonedDateTime.parse(timestamp);
-            Duration delta = Duration.between(now, sentAt);
-            if (delta.getSeconds() > 60)
-                throw new UnauthorisedException();
+        // Compare timestamp
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/London"));
+        ZonedDateTime sentAt = ZonedDateTime.parse(timestamp);
+        Duration delta = Duration.between(now, sentAt);
+        if (delta.getSeconds() > 60)
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
 
-            // Verify username, password and claim
-            dataManagerRepository.findById(username).ifPresentOrElse(dataManager -> {
-                if (!passwordEncoder.matches(password, dataManager.getPassword()))
-                    throw new UnauthorisedException();
+        // Verify username, password and claim
+        dataManagerRepository.findById(username).ifPresentOrElse(dataManager -> {
+            if (!passwordEncoder.matches(password, dataManager.getPassword()))
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
 
-                if (!claim.equals("admin")) {
-                    if (!dataManager.isSysadmin()) {
-                        campusRepository.findById(Long.parseLong(claim)).ifPresentOrElse(campus -> {
-                            if (!dataManager.getCampuses().contains(campus)) {
-                                throw new UnauthorisedException();
-                            }
-                        }, () -> {
-                            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-                        });
-                    }
-                } else if (!dataManager.isSysadmin())
-                    throw new UnauthorisedException();
-            }, () -> {
-                throw new UnauthorisedException();
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new AuthorizationServiceException("Could not decrypt credentials");
-        }
+            if (!claim.equals("admin")) {
+                if (!dataManager.isSysadmin()) {
+                    campusRepository.findById(Long.parseLong(claim)).ifPresentOrElse(campus -> {
+                        if (!dataManager.getCampuses().contains(campus)) {
+                            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+                        }
+                    }, () -> {
+                        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+                    });
+                }
+            } else if (!dataManager.isSysadmin())
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }, () -> {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        });
 
         authentication.setAuthenticated(true);
         return authentication;
